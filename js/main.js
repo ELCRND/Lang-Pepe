22a3f7a18f30777aed10c89da9b13c728243294d
ELCRND <jwhh2a@gmail.com>
2024-07-11 00:24:51 +0300

    modified:   css/styles.css 	modified:   css/styles.css.map 	modified:   index.html 	modified:   js/main.js 	deleted:    js/swiper.js 	modified:   public/img/about/about_ava_1_mobile.png 	new file:   public/img/buy/buy_img_mobile.png 	modified:   public/img/header/header_bg.png 	new file:   public/img/header/menu_bg.png 	new file:   public/img/home/home_bg_mobile.png 	modified:   scss/base/_base.scss 	modified:   scss/blocks/about.scss 	modified:   scss/blocks/buy.scss 	modified:   scss/blocks/footer.scss 	modified:   scss/blocks/header.scss 	modified:   scss/blocks/home.scss 	modified:   scss/blocks/roadmap.scss 	modified:   scss/blocks/tokenomics.scss

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
-// const menuBtn = document.querySelector(".menu-btn");
-// const menu = document.querySelector(".header__menu");
+const menuBtn = document.querySelector(".header__menu-btn");
+const menu = document.querySelector(".header__menu");
 const toggleBtns = document.querySelectorAll(".toggler__btn");
 const personsInfo = document.querySelectorAll(".person");
 const key = document.querySelector("#key");
 const tooltip = document.querySelector(".tooltip");
 
 // открыть/закрыть меню
-// menuBtn.addEventListener("click", () => {
-//   menu.classList.toggle("visible");
-//   menuBtn.classList.toggle("close");
-//   document.body.classList.toggle("scroll-off");
-// });
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("visible");
+  menuBtn.classList.toggle("close");
+  document.body.classList.toggle("scroll-off");
+});
 
 // закрытие меню при нажатии на ссылку
-// document.querySelectorAll(".menu__link").forEach((link) =>
-//   link.addEventListener("click", () => {
-//     menu.classList.remove("visible");
-//     menuBtn.classList.remove("close");
-//     document.body.classList.remove("scroll-off");
-//   })
-// );
+document.querySelectorAll(".menu__link").forEach((link) =>
+  link.addEventListener("click", () => {
+    menu.classList.remove("visible");
+    menuBtn.classList.remove("close");
+    document.body.classList.remove("scroll-off");
+  })
+);
+
+faqBtns.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    e.target.parentNode.classList.toggle("open");
+    e.target.classList.toggle("open");
+  })
+);
 
 // переключение контента в блоке about
 toggleBtns.forEach((btn) =>
   btn.addEventListener("click", (e) => {
-    const contentType = e.target.getAttribute("data-type");
+    console.log(e.target.parentNode.parentNode);
+    const contentType =
+      e.target.getAttribute("data-type") ||
+      e.target.parentNode.parentNode.getAttribute("data-type");
     toggleBtns.forEach((b) => b.classList.remove("active"));
     e.target.classList.add("active");
 
